d73e005f5366580ac93bbd0727626fea64d1ec0e
Dakota Clark <bannaray95@gmail.com>
2018-11-12 04:01:39 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -17,11 +17,15 @@ class School
         return y
       end
     end
-  
-  def sort
-    roster.sort_by do |x, y|
-      x
-    end
   end
+  
+  def sort 
+    nu_hash = {}
+ nu_hash = {}
+ roster.each do |x, y| 
+# #     nu_hash[x] = y.sort 
+# #   end 
+# #   nu_hash
+# # end 
 
 end
