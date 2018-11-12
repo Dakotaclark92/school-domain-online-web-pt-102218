615833570541eb248da4d3bdb08cc7e0ff3a5906
Dakota Clark <bannaray95@gmail.com>
2018-11-12 03:41:29 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,17 +1,7 @@
 class School
   
-  school.roster(name, grade) = []
   
-  def add_student
-    add_student = {}
-  end
-  
-  school.grade()
-  
-  school.sort
-  
-  
-  
+ 
 end
 
-school = School.new("Bayside High School")
+
