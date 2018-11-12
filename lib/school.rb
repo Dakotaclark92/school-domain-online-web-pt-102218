6af0e8ccb88a565c79b3562d3f46d254edaec2ec
Dakota Clark <bannaray95@gmail.com>
2018-11-12 03:51:27 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -18,9 +18,16 @@ class School
       end
     end
     
+    def sort
+      new_hash = {}
+      roster.each do |x, y|
+        new_hash[x] = y.sort
+      end
+      new_hash
+    end
+  end
   
   
- 
 end
 
 
